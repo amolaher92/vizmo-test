fc75a6034d64c55df16f01e1cd04e14699f8728c
Amol_Aher <amolaher92@gmail.com>
2022-03-26 18:39:56 +0530

    display blog component in view

---
+++ src/components/SimpleBlog.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="grid">
+        <div class="col-4" v-for="blog in allBlogs" :key="blog.id">
+            <Card v-if="blog.id <= 10">
+                <template #title>
+                    {{blog.title}}
+                </template>
+                <template #content>
+                    {{blog.body}}
+                </template>
+            </Card>
+        </div>
+    </div>
+</template>
+<script>
+//import { mapGetters, mapActions } from 'vuex'
+import Card from 'primevue/card';
+export default {
+    name: 'SimpleTodo',
+    components: {
+        Card
+    },
+    /* computed: mapGetters(['allBlogs']),
+     methods: {
+         ...mapActions(['fetchBlogs'])
+     },
+     created() {
+         this.fetchBlogs()
+     }*/
+    computed: {
+        allBlogs() {
+            return this.$store.getters.allBlogs
+        }
+    },
+    created() {
+        this.$store.dispatch('fetchBlogs')
+    },
+}
+/**
+ * 1) Way First (Indirect)
+ * import {mapGetters,mapActions} from 'vuex'
+ * computed : mapGetters(['allblogs']),
+ * methods:{
+ * 	...mapActions(['fetchBlogs'])
+ * },
+ * created(){
+ * 	this.fetchBlogs
+ * }
+ * --------------------------------------------
+ * 2) Way Second(direct)
+ * computed:{
+ * 	allBolgs(){
+ * 		return this.$store.getters.allBlogs
+ * 	}
+ * },
+ * created(){
+ * 	this.$store.dispatch('fetchBlogs')
+ * }
+ * */
+</script>
