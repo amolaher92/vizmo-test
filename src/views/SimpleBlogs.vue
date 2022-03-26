5a6faf80f33876a077ace51e2a330beccc734067
Amol_Aher <amolaher92@gmail.com>
2022-03-26 18:39:01 +0530

    created component and get store data to the display

---
+++ src/views/SimpleBlogs.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Simple blogs</h1>
+        <simple-blog></simple-blog>
+    </div>
+</template>
+<script>
+import SimpleBlog from '../components/SimpleBlog.vue'
+export default {
+    name: 'SimpleTodos',
+    components: {
+        SimpleBlog
+    }
+}
+</script>
